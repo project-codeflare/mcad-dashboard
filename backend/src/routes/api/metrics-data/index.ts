9070e51855174ccad082bf6ab6984a47a0badf09
charrell <charrell@redhat.com>
2023-06-12 10:16:06 -0700

    add shell file to get route

--- backend/src/routes/api/metrics-data/index.ts
+++ backend/src/routes/api/metrics-data/index.ts
@@ -2,7 +2,6 @@ import { secureRoute } from '../../../utils/route-security';
 import { KubeFastifyInstance, OauthFastifyRequest } from '../../../types';
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { metricsData } from './metricsData';
-import { getDirectCallOptions } from '../../../utils/directCallUtils';
 
 module.exports = module.exports = async (fastify: KubeFastifyInstance) => {
   fastify.post(
