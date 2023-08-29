018268b78903d314855f43f4f4efd171d53e4972
Maia Iyer <maia.raj.iyer@gmail.com>
2023-08-29 10:21:35 -0400

    Fixing shell scripts
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- backend/src/routes/api/mcad-prometheus/get-route.sh
+++ backend/src/routes/api/mcad-prometheus/get-route.sh
@@ -1,4 +1,4 @@
-if oc get route prometheus-portal -n odh -o jsonpath="{.spec.host}" ; then
+if [[ $(command -v oc) ]] ; then
 	oc get route prometheus-portal -n odh -o jsonpath="{.spec.host}" 
 else 
 	echo "prometheus-operated.odh.svc.cluster.local:9090"
--- backend/src/routes/api/metrics-data/get-route.sh
+++ backend/src/routes/api/metrics-data/get-route.sh
@@ -1,5 +1,5 @@
-if oc get route prometheus-k8s -n openshift-monitoring -o jsonpath="{.spec.host}" ; then
+if [[ $(command -v oc) ]] ; then # if oc exists, use this command - for local testing
 	oc get route prometheus-k8s -n openshift-monitoring -o jsonpath="{.spec.host}" 
-else 
+else # if oc does not exist
 	echo "prometheus-operated.openshift-monitoring.svc.cluster.local:9090"
 fi
